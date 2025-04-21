2ddefec89ed94a969c188c955fbbb0d2b32bba77
PandaDev888 <luis.grau1130@gmail.com>
2025-04-21 09:59:15 +0200

    background work imrovement

--- single_task_coder.py
+++ single_task_coder.py
@@ -31,7 +31,7 @@ execute_file_name = os.getenv("EXECUTE_FILE_NAME", "main.py")
 
 
 def run_clean_coder_pipeline(task: str, work_dir: str, doc_harvest: bool = False):
-    researcher = Researcher(work_dir)
+    researcher = Researcher()
     files, image_paths = researcher.research_task(task)
     # documentation = None
     # if doc_harvest:
